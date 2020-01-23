7b1a55a3d32f9eb3500a4265a1ab09dd9ae33323
Tiana Aiyemo <chrissetiana@gmail.com>
2020-01-23 15:33:39 +0100

    Added play view controller

---
+++ pitchperfect/PlayViewController.swift
@@ -0,0 +1,32 @@
+//
+//  PlayViewController.swift
+//  pitchperfect
+//
+//  Created by Chrisse Tiana on 23/01/2020.
+//  Copyright © 2020 Chrisse Tiana. All rights reserved.
+//
+
+import UIKit
+
+class PlayViewController: UIViewController {
+    
+    var recordedAudioURL: URL!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
